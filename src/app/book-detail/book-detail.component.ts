a76660fa227a8d383db708de6a80b780619f7b01
Kishor Kharade <kishor.kharade@atmecs.com>
2018-02-15 23:33:28 +0530

    Added code to delete book

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
-  styleUrls: ['./book-detail.component.css']
+  styleUrls: ['./book-detail.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BookDetailComponent implements OnInit {
 
   book: any = {};
 
   constructor(private route: ActivatedRoute,
-              private http: HttpClient) { }
+              private http: HttpClient,
+              private router: Router) { }
 
   ngOnInit() {
     this.getBookDetail(this.route.snapshot.params['id']);
@@ -22,5 +24,14 @@ export class BookDetailComponent implements OnInit {
     this.http.get('/book/' + id).subscribe(data => {
       this.book = data;
     });
+  }
+
+  deleteBook(id) {
+    this.http.delete('/book/' + id)
+      .subscribe(res => {
+        this.router.navigate(['/books']);
+      }, (err) => {
+        console.log(err);
+      });
   }
 }
